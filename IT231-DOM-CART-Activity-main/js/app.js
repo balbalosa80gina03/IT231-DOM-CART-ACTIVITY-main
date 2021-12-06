898946703d767de344ec07477effa982f58b9dc5
balbalosa80gina03 <95342536+balbalosa80gina03@users.noreply.github.com>
2021-12-06 20:49:53 +0800

    Add files via upload

---
+++ IT231-DOM-CART-Activity-main/js/app.js
@@ -0,0 +1,8 @@
+const courses = document.querySelector('#courses-list'),
+shoppingCartContent = document.querySelector('#cart-content tbody'),
+clearCartBtn = document.querySelector('#clear-cart');
+
+
+
+
+
